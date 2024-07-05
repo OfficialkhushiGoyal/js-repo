5a41f72413465fa182b954567fa4f34172daeeb1
Khushi Goyal <khushigoyalofficial@gmail.com>
2024-07-05 17:40:10 +0000

    coomit done

---
+++ .devcontainer/basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123456
+let accountEmail = "khushi@123.com"
+var password = "67892"
+accountCity = "jaipur"
+
+// accountId = 2 (not allow)
+//accountEmail = "imkhushigoyal@4567" (changeble)
+//password = "radharani" (changeble)
+
+//console.log(password);
+
+console.table([accountEmail,accountId,password,accountCity])
