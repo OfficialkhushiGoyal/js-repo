eac3abe4a1f828c5e0043dad9291a39fe1287f53
Khushi Goyal <khushigoyalofficial@gmail.com>
2024-07-06 16:32:14 +0000

    changes

--- .devcontainer/basics/01_variables.js
+++ .devcontainer/basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 123456
 let accountEmail = "khushi@123.com"
 var password = "67892"
 accountCity = "jaipur"
+let AccountState ;
 
 // accountId = 2 (not allow)
 //accountEmail = "imkhushigoyal@4567" (changeble)
@@ -9,4 +10,6 @@ accountCity = "jaipur"
 
 //console.log(password);
 
-console.table([accountEmail,accountId,password,accountCity])
+/* in var there is a issue of a block scope and functional scope */
+
+console.table([accountEmail,accountId,password,accountCity, AccountState])
