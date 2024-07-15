37af6f9c03422261b62efce393fafc1e650ca96c
Khushi Goyal <khushigoyalofficial@gmail.com>
2024-07-15 17:08:32 +0000

    condition

---
+++ .devcontainer/basics/03_conditions.js
@@ -0,0 +1,9 @@
+//Write a program that determines whether a given number is positive or negative.
+
+let a = -2;
+if (a<=0){
+    console.log("value is negative ");
+}
+else{
+    console.log("value is positive")
+}
