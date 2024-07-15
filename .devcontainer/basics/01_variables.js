4d462c243f8cb0a76f60d54eed5b036030550d25
Khushi Goyal <khushigoyalofficial@gmail.com>
2024-07-15 17:00:39 +0000

    change

--- .devcontainer/basics/01_variables.js
+++ .devcontainer/basics/01_variables.js
@@ -13,3 +13,19 @@ let AccountState ;
 /* in var there is a issue of a block scope and functional scope */
 
 console.table([accountEmail,accountId,password,accountCity, AccountState])
+
+let x = 5; 
+let y = 7;
+let z = x+y;
+console.log(z); // in let we have block scope
+
+var a= 10;
+var b = 20;
+var c = a+b;
+console.log(c); // in var there is no block scope
+
+const i= 4;
+const j= 6;
+const k = i+j;
+console.log(k); /*in const there no changeble value if we create a variable then 
+                  we can't able to change or call that variable again*/ 
