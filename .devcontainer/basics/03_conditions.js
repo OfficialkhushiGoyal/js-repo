e8fce1e334f6d13012f6a213a595cb42aa071603
Khushi Goyal <khushigoyalofficial@gmail.com>
2024-07-17 16:45:10 +0000

    change

--- .devcontainer/basics/03_conditions.js
+++ .devcontainer/basics/03_conditions.js
@@ -7,3 +7,32 @@ if (a<=0){
 else{
     console.log("value is positive")
 }
+
+//Write a program to determine the greater of two numbers
+
+let num1 = 30;
+let num2 = 20;
+
+if (num1 > num2) {
+  console.log("num1 is greater");
+} else {
+  console.log("num2 is greater");
+}
+
+//Write a program that transforms a numerical grade to a letter grade (e.g grade 10 should display “A”).
+
+var b = 70;
+var grade;
+
+if (b >= 90) {
+  grade = "A";
+} else if (a >= 80) {
+  grade = "B";
+} else if (a >= 70) {
+  grade = "C";
+} else if (a >= 60) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+console.log("grade" + grade);
